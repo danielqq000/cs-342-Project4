b568731c3dc909acdd5c23face3785d1b97e090e
danielqq000 <danielqq000@yahoo.com>
2023-04-27 16:40:50 -0500

    v1.1

--- src/main/java/GuiServer.java
+++ src/main/java/GuiServer.java
@@ -38,7 +38,6 @@ public class GuiServer extends Application{
 	
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		launch(args);
 	}
 
@@ -48,88 +47,88 @@ public class GuiServer extends Application{
 		primaryStage.setTitle("The Networked Client/Server GUI Example");
 		
 		this.serverChoice = new Button("Server");
-		this.serverChoice.setStyle("-fx-pref-width: 300px");
-		this.serverChoice.setStyle("-fx-pref-height: 300px");
-		
-		this.serverChoice.setOnAction(e->{ primaryStage.setScene(sceneMap.get("server"));
-											primaryStage.setTitle("This is the Server");
-				serverConnection = new Server(data -> {
-					Platform.runLater(()->{
-						listItems.getItems().add(data.toString());
-					});
+		this.serverChoice.setStyle("-fx-pref-width: 150px; -fx-pref-height: 80px");
 
+		this.serverChoice.setOnAction(e->{ 
+			primaryStage.setScene(sceneMap.get("server"));
+			primaryStage.setTitle("Server");
+			serverConnection = new Server(data -> {
+				Platform.runLater(()->{
+					listItems.getItems().add(data.toString());
 				});
-											
+
+			});
+
 		});
-		
-		
+
+
 		this.clientChoice = new Button("Client");
-		this.clientChoice.setStyle("-fx-pref-width: 300px");
-		this.clientChoice.setStyle("-fx-pref-height: 300px");
-		
-		this.clientChoice.setOnAction(e-> {primaryStage.setScene(sceneMap.get("client"));
-											primaryStage.setTitle("This is a client");
-											clientConnection = new Client(data->{
-							Platform.runLater(()->{listItems2.getItems().add(data.toString());
-											});
-							});
-							
-											clientConnection.start();
+		this.clientChoice.setStyle("-fx-pref-width: 150px; -fx-pref-height: 80px");
+
+		this.clientChoice.setOnAction(e->{
+			primaryStage.setScene(sceneMap.get("client"));
+			primaryStage.setTitle("Client");
+			clientConnection = new Client(data->{
+				Platform.runLater(()->{listItems2.getItems().add(data.toString());
+				});
+			});
+
+			clientConnection.start();
 		});
-		
-		this.buttonBox = new HBox(400, serverChoice, clientChoice);
+
+		this.buttonBox = new HBox(80, serverChoice, clientChoice);
 		startPane = new BorderPane();
-		startPane.setPadding(new Insets(70));
+		startPane.setPadding(new Insets(50));
 		startPane.setCenter(buttonBox);
-		
-		startScene = new Scene(startPane, 800,800);
-		
+
+		startScene = new Scene(startPane, 400, 200);
+
 		listItems = new ListView<String>();
 		listItems2 = new ListView<String>();
-		
+
 		c1 = new TextField();
 		b1 = new Button("Send");
 		b1.setOnAction(e->{clientConnection.send(c1.getText()); c1.clear();});
-		
+
 		sceneMap = new HashMap<String, Scene>();
-		
+
 		sceneMap.put("server",  createServerGui());
 		sceneMap.put("client",  createClientGui());
-		
-		primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
-            @Override
-            public void handle(WindowEvent t) {
-                Platform.exit();
-                System.exit(0);
-            }
-        });
-		
 
-		
+		primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+			@Override
+			public void handle(WindowEvent t) {
+				Platform.exit();
+				System.exit(0);
+			}
+		});
+
+
+
 		primaryStage.setScene(startScene);
 		primaryStage.show();
-		
+
 	}
-	
+
 	public Scene createServerGui() {
-		
+
 		BorderPane pane = new BorderPane();
 		pane.setPadding(new Insets(70));
 		pane.setStyle("-fx-background-color: coral");
-		
+
 		pane.setCenter(listItems);
-	
-		return new Scene(pane, 500, 400);
-		
-		
+
+		return new Scene(pane, 600, 600);
+
+
 	}
-	
+
 	public Scene createClientGui() {
 		//clientList = new ComboBox();
 		//clientSelect = new HBox(10, clientList, b1);
 		clientBox = new VBox(10, c1, b1, listItems2);
 		clientBox.setStyle("-fx-background-color: navy");
-		return new Scene(clientBox, 400, 300);
+		return new Scene(clientBox, 600, 600);
 	}
 
 }
